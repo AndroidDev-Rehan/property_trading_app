38c8f42211e37687efe9499109395bd3111c9e73
rehan <rehanjutt005@gmail.com>
2022-07-26 17:24:48 +0500

    ..

--- lib/new_UI_screens/splash/splash_screen.dart
+++ lib/new_UI_screens/splash/splash_screen.dart
@@ -1,10 +1,6 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:page_transition/page_transition.dart';
 import '../../../utils/app-color.dart';
-
-import '../../../utils/text_style.dart';
 import '../welcome/welcome_screen.dart';
 
 class CustomSplashScreen extends StatefulWidget {
@@ -26,25 +22,25 @@ class _CustomSplashScreenState extends State<CustomSplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: darkMain,
-        height: Get.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/glogo1.png',height: Get.height*0.4,width: Get.width,fit: BoxFit.fill,),
-              SizedBox(height: 20,),
-              Text('Above  The Crowd In Property',style: TextStyle(fontSize: 18,color: Colors.white,fontStyle: FontStyle.italic),)
-            ],
-          ),
+      backgroundColor: darkMain,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // const Text('Above Everyone Hello HI',style: TextStyle(fontSize: 18,color: Colors.white,fontStyle: FontStyle.italic),),
+          // const SizedBox(height: 20,),
+          Image.asset('assets/images/glogo1.png',),
+          const SizedBox(height: 20,),
+          const Text('Above the crowd in Property',style: TextStyle(fontSize: 18,color: Colors.white,fontStyle: FontStyle.italic),)
+        ],
       ),
 
     );
   }
+
   screenFunction() async {
-    await Future.delayed(const Duration(seconds:8));
-    return (
-        Get.off(()=> WelcomeScreen()));
+    await Future.delayed(const Duration(seconds: 3));
+    Get.off(()=> const WelcomeScreen());
   }
+
 }
 
